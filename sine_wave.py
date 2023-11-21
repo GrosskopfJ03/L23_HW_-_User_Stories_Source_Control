33703263e5cf4c010c19af4ea51a23de2cae803b
Jacob Grosskopf <Jayrayg03@gmail.com>
2023-11-20 21:16:27 -0500

    Changed frequency in the plot

--- sine_wave.py
+++ sine_wave.py
@@ -2,15 +2,17 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
+# Modify the frequency in the plot_sine_wave function
 def plot_sine_wave():
-    x = np.linspace(0, 2 * np.pi, 1000)  # Generate x values from 0 to 2*pi
-    y = np.sin(x)  # Calculate corresponding y values using the sine function
+    x = np.linspace(0, 2 * np.pi, 1000)
+    y = np.sin(2 * x)  # Change the frequency by multiplying x by 2
 
-    plt.plot(x, y)  # Plot the sine wave
-    plt.title('Sine Wave')  # Set the plot title
-    plt.xlabel('Time')  # Set the x-axis label
-    plt.ylabel('Amplitude')  # Set the y-axis label
-    plt.show()  # Display the plot
+    plt.plot(x, y)
+    plt.title('Modified Sine Wave')  # Update the title if needed
+    plt.xlabel('Time')
+    plt.ylabel('Amplitude')
+    plt.show()
+
 
 if __name__ == "__main__":
     plot_sine_wave()
